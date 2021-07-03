e6bb9556f04d9e483fcd7db344cca4f5167c1b73
Remi <i.eris0023@gmail.com>
2021-07-03 18:08:19 +0100

    cars page first draft

--- script.js
+++ script.js
@@ -12,6 +12,64 @@ $(document).ready(function(){
         })
   });
 
+
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+//change the text in the caption of the picture - depending on what the pic is
+function changeTextAuto(){
+    var text = document.getElementsByClassName("text");
+    var car_details = document.getElementById("details").textContent;
+
+    for(var i = 0; i < text.length; i++){
+      text[i].innerHTML = car_details;
+    }    
+}
+
+changeTextAuto();
+
+
+//change color of sold status
+function changeColor(){
+  var status = document.getElementById("status");
+
+  if(status.innerHTML == "SOLD"){
+    status.style.color = "green";
+  } else if(status.innerHTML == "FOR SALE") {
+    status.style.color = "red";
+  } else {
+    status.style.color = "black";
+  }
+}
+
+changeColor();
+
                                    
 
   
