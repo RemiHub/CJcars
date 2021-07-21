26ac6d506a6c3340799d29a44f07cef6ff61c2b7
Remi <i.eris0023@gmail.com>
2021-07-21 02:41:37 +0100

    form car make and model - completed w dynamics

--- carScript.js
+++ carScript.js
@@ -119,3 +119,180 @@ window.addEventListener('click', e =>
   e.target == modal ? modal.style.display = "none" : false
   // e.target == modal ? modal.classList.remove('show-modal') : false
 );
+
+
+
+
+
+
+const carMake = document.querySelectorAll('#car-make'); 
+let carData = [];
+
+//go through the cars and place in array to use later
+carMake.forEach(e=>{
+  var carMakeType = e.innerHTML;
+  const carArr = carMakeType.split(", ");
+  carData.push(carArr);
+})
+
+
+
+function makeDropDown(data, filtering){
+
+  let filteredArray = data.filter(r=>r[0] === filtering);
+
+  // const uniqueOptions = new Set();
+  // filteredArray.forEach(r=>uniqueOptions.add(r[1]));
+
+  const uniqueList =  getUniqueValues(filteredArray, 1); //[...uniqueOptions];
+
+  const selectedOptions = document.getElementById("car-model");
+
+  // selectedOptions.innerHTML = "";
+
+  // uniqueList.forEach(item=>{
+  //   const option = document.createElement("option");
+  //   option.textContent = item;
+  //   selectedOptions.appendChild(option);
+// });
+
+  populateDropDown(selectedOptions, uniqueList);
+
+}
+
+// makeDropDown(carData, "BMW");
+
+function applyDropDown(){
+  let selectedOptions2 = document.getElementById("car-makes").value;
+  // console.log(selectedOptions2)
+  makeDropDown(carData, selectedOptions2)
+}
+
+// applyDropDown();
+
+function afterDocLoads(){
+  populateFirstDropDown();
+  applyDropDown()
+}
+
+function getUniqueValues(data, index){
+  
+  const uniqueOptions = new Set();
+  data.forEach(r=>uniqueOptions.add(r[index]));
+  return [...uniqueOptions];
+}
+
+function populateFirstDropDown(){
+  const uniqueList = getUniqueValues(carData,0);
+  const el = document.getElementById("car-makes");
+  populateDropDown(el, uniqueList);
+}
+
+
+function populateDropDown(el, listAsArr){
+    el.innerHTML = "";
+
+    listAsArr.forEach(item=>{
+    const option = document.createElement("option");
+    option.textContent = item;
+    el.appendChild(option);
+});
+}
+
+document.getElementById("car-makes").addEventListener("change", applyDropDown);
+document.addEventListener("DOMContentLoaded", afterDocLoads);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//find the names of car makes in showroom and add to form
+function addCarMakeToForm(){
+
+  var select = document.querySelectorAll('#car-make');
+  var options = [];
+  
+  
+  select.forEach(e=>{
+    var words = e.innerHTML;
+    options.push(words);
+  })
+
+  //arrange the array to not have duplicates and in alphabetical order
+  var finalArr = options;
+  // var uniqueArr = [...new Set(finalArr)];
+  // uniqueArr.sort();
+
+
+  //make a select element and append the above array
+  var formBox = document.querySelector('label');
+  var selectedListMake = document.createElement('select');
+  selectedListMake.id = 'car-makes';
+  formBox.appendChild(selectedListMake);
+
+  //loop over the new array and append the array into the options for the select form
+  for(var i =0; i < finalArr.length; i++){
+    // console.log(finalArr[i]);
+    var option = document.createElement("option");
+    option.value = finalArr[i];
+    option.textContent = finalArr[i];
+    selectedListMake.appendChild(option);
+    // console.log(option)
+    
+  }
+}
+
+// addCarMakeToForm();
+
+
+
+function addCarModelToForm(){
+
+  var select2 = document.querySelectorAll('#car-mode');
+  var options2 = [];
+
+  select2.forEach(e=>{
+    var words2 = e.innerHTML;
+    options2.push(words2);
+  })
+
+  var finalArr2 = options2;
+  var uniqueArr2 = [...new Set(finalArr2)];
+  uniqueArr2.sort();
+
+  var formBox2 = document.querySelector('.car-model');
+  var selectedListModel = document.createElement('select');
+  selectedListModel.id = 'car-model';
+  formBox2.appendChild(selectedListModel);
+
+  
+  // console.log(formBox);
+
+  for(var i =0; i < uniqueArr2.length; i++){
+    // console.log(finalArr[i]);
+    var option2 = document.createElement("option");
+    option2.value = uniqueArr2[i];
+    option2.textContent = uniqueArr2[i];
+    selectedListModel.appendChild(option2);
+  }
+
+  
+}
+
+// addCarModelToForm();
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -138,3 +138,4 @@ function changeColor(){
 changeColor();
 
 
+
